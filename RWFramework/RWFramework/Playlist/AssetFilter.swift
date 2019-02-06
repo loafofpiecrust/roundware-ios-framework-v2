682a47f9ca30bb9f108b8214e30e219c5559c7cb
Halsey Burgund <halsey@halseyburgund.com>
2019-02-05 19:47:39 -0500

    fix tag filters by using tag_ids instead of uiitem_ids

--- RWFramework/RWFramework/Playlist/AssetFilter.swift
+++ RWFramework/RWFramework/Playlist/AssetFilter.swift
@@ -72,12 +72,12 @@ struct AllAssetFilters: AssetFilter {
 
 struct AnyTagsFilter: AssetFilter {
     func keep(_ asset: Asset, playlist: Playlist, track: AudioTrack) -> AssetPriority {
-        // List of tags to listen for.
-        guard let listenTags = RWFramework.sharedInstance.getListenIDsSet()
+        // List of tag_ids to listen for.
+        guard let listenTagIDs = RWFramework.sharedInstance.getSubmittableListenTagIDsSet()
             else { return .lowest }
 
         let matches = asset.tags.contains { assetTag in
-            listenTags.contains(assetTag)
+            listenTagIDs.contains(assetTag)
         }
         // matching only by tag should be the least important filter.
         return matches ? .lowest : .discard
@@ -86,12 +86,12 @@ struct AnyTagsFilter: AssetFilter {
 
 struct AllTagsFilter: AssetFilter {
     func keep(_ asset: Asset, playlist: Playlist, track: AudioTrack) -> AssetPriority {
-        // List of tags to listen for.
-        guard let listenTags = RWFramework.sharedInstance.getListenIDsSet()
+        // List of tag_ids to listen for.
+        guard let listenTagIDs = RWFramework.sharedInstance.getSubmittableListenTagIDsSet()
             else { return .lowest }
 
         let matches = asset.tags.allSatisfy { assetTag in
-            listenTags.contains(assetTag)
+            listenTagIDs.contains(assetTag)
         }
 
         return matches ? .lowest : .discard
